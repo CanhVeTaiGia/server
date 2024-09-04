9d32e68cd24431470a1c3cc3e7abb263210e6999
CanhVeTaiGia <lvy16012005@gmail.com>
2024-09-04 13:48:30 +0700

    add

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const jsonserver = require('json-server');
-// Tạo máy chủ
-const server = jsonserver.create();
-const router = jsonserver.router('db.json');
-const middlewares = jsonserver.defaults();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
+
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/users/:resource/:id/show": "/:resource/:id",
+    "/products/:resource/:id/show": "/:resource/:id",
+    "/carts/:resource/:id/show": "/:resource/:id",
+    "/orders/:resource/:id/show": "/:resource/:id",
+  })
+);
 server.use(router);
 
-
 server.listen(3000, () => {
-    console.log("https://localhost:3000");
-    
+  console.log("JSON Server is running");
 });
-module.exports = server
+
+// Export the Server API
+module.exports = server;
